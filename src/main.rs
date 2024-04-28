7bc428c74a8ab169e9b689c8132e14e952ae516a
Kayxue <s0990006@gmail.com>
2024-04-28 23:24:54 +0800

    2024/4/28 Test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use ntex::web;
+
+#[web::get("/")]
+async fn hello() -> impl web::Responder{
+    "Welcome to the API"
+}
+
+#[ntex::main]
+async fn main()->std::io::Result<()> {
+    web::HttpServer::new(|| {
+        web::App::new()
+        .service(hello)
+    })
+    .bind(("localhost",80))?
+    .run()
+    .await
 }
